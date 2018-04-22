043b564129ed29d7ec3e030f09b704672fb27e72
Marie Sonko <mariesonko62@gmail.com>
2018-04-22 20:33:36 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -31,4 +31,15 @@ def merge_data (keys, data)
     array.select {|element| element[:temperature] == "cool"}
   end
 
-  
+  def organize_schools(schools)
+    organized_schools ={}
+    schools.each do |actual_school, info|
+      info.each do |place, location|
+      if organized_schools.has_key? (location)
+        organized_schools [location] << actual_school
+      else organized_schools[location] = [actual_school]
+      end
+    end
+  end
+      organized_schools
+    end
