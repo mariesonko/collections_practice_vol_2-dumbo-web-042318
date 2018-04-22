043c036e3e0cf1451f7db5e499d471aea31b0669
Marie Sonko <mariesonko62@gmail.com>
2018-04-22 19:50:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,11 +1,3 @@
 def begins_with_r(array)
-  array.each do |x|
-    x.each do |y|
-      if y[1][0] == "r"
-        true
-      else false
-end
-end
-end
-end
-end
+array.all? {|element| element.include ? "r"}
+end 
