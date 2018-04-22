ce4c2cef911f2de1c91cf39f4287ff3b752d18bc
Marie Sonko <mariesonko62@gmail.com>
2018-04-22 19:58:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,7 +1,7 @@
 def begins_with_r (array)
- array.all? { |e| e.start_with? "r" }
+ array.all? { |element| element.start_with? "r" }
 end
 
 def contain_a(array)
   array.select {|element| element.include? "a"}
-end 
+end
